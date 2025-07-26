d69a402cac00c84f6a6a329575c552f56af45d12
Luis Perez <twetter@gmail.com>
2025-07-26 18:35:48 +0000

    : Comportamiento correcto de la barra lateral auto-ocultable
    ---
    
    **Obj

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -7,7 +7,7 @@ import { SidebarProvider, Sidebar, SidebarInset } from '@/components/ui/sidebar'
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
      <SidebarProvider>
-      <Sidebar>
+      <Sidebar collapsible="icon">
         <SidebarNav />
       </Sidebar>
       <SidebarInset>
